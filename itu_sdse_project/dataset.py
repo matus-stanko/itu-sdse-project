150d94618b06f4d59fb1cff040cc34812fa933f1
lucienavratilova <luna@itu.dk>
2025-11-25 16:39:54 +0100

    chore: added comments to dataset.py

--- itu_sdse_project/dataset.py
+++ itu_sdse_project/dataset.py
@@ -20,32 +20,32 @@ def read_data(data_path=RAW_DATA_PATH, min_date=MIN_DATE, max_date=MAX_DATE):
     # Create artifacts dir
     os.makedirs("artifacts",exist_ok=True)
 
-    warnings.filterwarnings('ignore')
-    pd.set_option('display.float_format',lambda x: "%.3f" % x)
+    warnings.filterwarnings('ignore') # Ignore warnings
+    pd.set_option('display.float_format',lambda x: "%.3f" % x) # Floats will display with 3 decimals
 
     data = pd.read_csv(data_path)
 
-    if not max_date:
+    if not max_date: # If max date not provided, set it to today's date
         max_date = pd.to_datetime(datetime.datetime.now().date()).date()
-    else:
+    else: # Otherwise convert given max date into datetime obj
         max_date = pd.to_datetime(max_date).date()
 
-    min_date = pd.to_datetime(min_date).date()
+    min_date = pd.to_datetime(min_date).date() # Do the same to min date
 
-    # Time limit data
+    # Time limit data (filter rows so that its between min and max)
     data["date_part"] = pd.to_datetime(data["date_part"]).dt.date
     data = data[(data["date_part"] >= min_date) & (data["date_part"] <= max_date)]
 
-    min_date = data["date_part"].min()
-    max_date = data["date_part"].max()
-    date_limits = {"min_date": str(min_date), "max_date": str(max_date)}
+    min_date = data["date_part"].min() # Recalculate min and max data
+    max_date = data["date_part"].max() # after filtering
+    date_limits = {"min_date": str(min_date), "max_date": str(max_date)} # Store
 
     with open("./artifacts/date_limits.json", "w") as f:
-        json.dump(date_limits, f)    
+        json.dump(date_limits, f) # Save the artifact
 
-    print('Data successfully red, time filtered and saved as raw_data_framed.csv')
+    #print('Data successfully read, time filtered and saved as raw_data_framed.csv')
 
-    # save data into artifacts
+    # Save data into artifacts
     data.to_csv('./artifacts/raw_data_framed.csv', index=False)
 
 
